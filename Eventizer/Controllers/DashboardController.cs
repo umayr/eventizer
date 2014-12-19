8911111e8cc0ed2698381d02a16e9992eb3f4928
Umayr Shahid <umayrr@hotmail.co.uk>
2014-12-19 18:39:39 +0500

    controller for dashboard

--- Eventizer/Controllers/DashboardController.cs
+++ Eventizer/Controllers/DashboardController.cs
@@ -19,7 +19,20 @@ namespace Eventizer.Controllers
             {
                 Current C = new Current();
                 C.Employee = (Employee)Session["Employee"];
-                ViewBag.Feeds = Feed.FetchAllFeeds();
+                List<Feed> Feeds = Feed.FetchAllFeeds();
+                if (C.Employee.Designation == "Manager")
+                {
+                    ViewBag.Feeds = Feeds;
+                }
+                else
+                {
+                    ViewBag.Feeds =
+                        (from feed in Feeds
+                        where feed.AssignedTo.ID == C.Employee.ID
+                        orderby feed.DateCreated descending
+                        select feed).ToList<Feed>();
+                }
+
                 return View(C);
             }
             else { return RedirectToAction("Index", "Home"); }
